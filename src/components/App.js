5379299a54079d5d8a9194a9dbc3b6f6ae6d2d2e
Peter <peter.prentiss@gmail.com>
2017-12-03 20:56:44 -0600

    Small updates.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import UserList from './UserList';
 
 export default class App extends Component {
     render() {
         return (
-            <div>React/Redux simple starter</div>
+            <div>
+                <UserList />
+            </div>
         );
     }
 }
